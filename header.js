9f7088adb9b45171b7e202f557fe7ca60b0d36b7
Cavid Nagiyev <cavid.nagiyev7@gmail.com>
2021-03-03 19:02:32 +0400

    First commit

---
+++ header.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    let generalSearch = $("#generalSearch").height();
+    let lupaHeight = (generalSearch - $("#magnifyingGlass").height())/2
+    $("#magnifyingGlass").css({
+        marginLeft: "15px",
+        marginTop: `${lupaHeight}px`
+    })
+
+    var searchCharacter = $("#generalSearch").val().length;
+    $("#generalSearch").on('keyup', function(){
+        searchCharacter = $("#generalSearch").val().length;
+
+        if (searchCharacter >= 1){
+            $("#generalSearch").removeClass('searchbarColorChangeReverse')
+            $("#generalSearch").addClass('searchbarColorChangeForward')
+        }else{
+            $("#generalSearch").removeClass('searchbarColorChangeForward')
+            $("#generalSearch").addClass('searchbarColorChangeReverse')
+        }
+    })
+    
+    // $(".headerLinks li").on('mouseenter', function(){
+    //     $(this).css({
+    //         backgroundColor: "#4da8da"
+    //     })
+    // })
+    //     $(".headerLinks li").on('mouseleave', function(){
+    //     $(this).css({
+    //         backgroundColor: "transparent"
+    //     })
+    // })
+})
