9abc8f8280b52717a34b1f4a434bea9fd05011c6
Cavid Nagiyev <cavid.nagiyev7@gmail.com>
2021-03-24 14:44:15 +0400

    Autocomplete

--- index.js
+++ index.js
@@ -13,78 +13,136 @@ $(document).ready(function(){
         pageDots: false
 });
 
-
+//STYLE
 $(".bigSearchWrapper").css({
     top: $(".bigSearchWrapper").parent().height()/2 - $(".bigSearchWrapper").height()/2
 });
 
 
 
-$('.where ul li').on('click', function(event){
-  event.preventDefault()
-  $(this).parent().parent().find('input').attr('value', `${$(this).text()}`)
-})
 
-$(".where").on('focusout', function(){
-    if ($(this).find("input").val().length == 0){
-        $(this).find(".floater").css({
-            transition: 'transform 0.1s',
-            transform: 'translate(0, 0)',
-            fontSize: "1rem",
-            opacity: '1'
-        })
-    }
-})
-$(".where").on('mousedown', function(){
-    $(this).find(".floater").css({
-        transition: 'transform 0.1s',
-        transform: 'translate(0, -15px)',
-        fontSize: '0.8rem',
-        opacity: '0.8'
+//AUTOCOMPLETE SEARCH
+const carMakeList = [
+  'Acura', 'Alfa Romeo', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Buick', 'Cadillac', 'Changan', 'Chevrolet', 'Chrysler', 'Citroen', 'Dacia', 'Daewoo', 'Dodge', 'Ferrari', 'Fiat', 'Ford', 'GMC', 'Honda', 'Hummer', 'Hyundai','Infiniti', 'Jaguar', 'Jeep', 'KIA', 'Lamborghini', 'Land Rover', 'Lexus', 'Lincoln', 'Maserati', 'Mazda', 'Mercedes', 'Mitsubishi', 'Nissan', 'Opel', 'Peugeout', 'Pontiac', 'Porsche', 'Renault', 'Rolls-Royce', 'Saab', 'Seat', 'Skoda', 'Smart', 'Subaru', 'Tesla', 'Toyota', 'Volkwagen', 'Volvo'
+]
+
+//Make autocomplete search
+
+let makeInput = $('#make')
+let makeList = $('.makeList')
+
+makeInput.on('keyup', (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if(userData){
+    emptyArray = carMakeList.filter((data) => {
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
     })
-    $(this).find('ul').css({display: 'block'})
-})
+    emptyArray = emptyArray.map((data) => {
+      return data = '<li>'+ data +'</li>'
+    })
+    makeList.css({display: 'block'})
+    showMake(emptyArray)
 
-
-
-
-
-
-
-})
-
-let where = document.querySelector('.where')
-where.addEventListener('keyup', function(){
-    if (this.children[0].id === 'make'){
-      myFunction('make')
-    }else if (this.children[0].id === 'model'){
-      myFunction('model')
-    }
-})
-
-
-//Search function through list
-function myFunction(makeormodel) {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  if (makeormodel === 'make'){
-      input = document.getElementById('make');
-      ul = document.querySelector('.makeList')
-  }else if (makeormodel === 'model'){
-      input = document.getElementById('model')
-      ul = document.querySelector('.modelList')
+    $('.makeList li').on('click', function(e){
+      $('#make').val(`${e.target.innerHTML}`)
+      $('#make').attr('value', `${e.target.innerHTML}`)
+      makeList.css({display: 'none'})
+      modelSearch(e.target.innerHTML)
+    })
+          //Hover gray
+          $('.makeList li').on('mouseenter', function(){
+            $(this).addClass('hover')
+          })
+          $('.makeList li').on('mouseleave',function(){
+            $(this).removeClass('hover')
+          })
+  }else{
+    makeList.css({display: 'none'})
   }
-  filter = input.value.toUpperCase();
-  li = ul.getElementsByTagName('li');
+  
+})
 
-    for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+function showMake(list){
+  let listData;
+  if(!list.length){
+    userValue = makeInput.value;
+    listData = '<li>'+ userValue + '</li>'
+  }else{
+    listData = list.join('')
   }
-
+  makeList.html(listData)
 }
+
+
+//Model autocomplete search
+
+function modelSearch(makebrand){
+  const acuraModels = [
+    'ILX', 'NSX', 'RLX', 'TLX', 'MDX', 'RDX', 'CSX', 'TL'
+  ]
+  const alfaromeoModels = [
+    '156', '4C', 'GTV', 'Guilietta', 'Mito', 'Stelvio'
+  ]
+  let carModelList = []
+
+  switch(makebrand){
+    case 'Acura':
+      carModelList = acuraModels
+      break;
+    case 'Alfa Romeo':
+      carModelList = alfaromeoModels
+      break;
+  }
+
+let modelInput = $('#model')
+let modelList = $('.modelList')
+let modelListItem = $('.modelList li')
+
+modelInput.on('keyup', (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if(userData){
+    emptyArray = carModelList.filter((data) => {
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    })
+    emptyArray = emptyArray.map((data) => {
+      return data = '<li>'+ data +'</li>'
+    })
+    modelList.css({display: 'block'})
+    showMake(emptyArray)
+
+    $('.modelList li').on('click', function(e){
+      $('#model').val(`${e.target.innerHTML}`)
+      $('#model').attr('value', `${e.target.innerHTML}`)
+      modelList.css({display: 'none'})
+    })
+
+            //Hover gray
+          $('.modelList li').on('mouseenter', function(){
+            $(this).addClass('hover')
+          })
+          $('.modelList li').on('mouseleave',function(){
+            $(this).removeClass('hover')
+          })
+    
+  }else{
+    modelList.css({display: 'none'})
+  }
+  
+})
+
+function showMake(list){
+  let listData;
+  if(!list.length){
+    userValue = modelInput.value;
+    listData = '<li>'+ userValue + '</li>'
+  }else{
+    listData = list.join('')
+  }
+  modelList.html(listData)
+}
+}
+
+})
+
